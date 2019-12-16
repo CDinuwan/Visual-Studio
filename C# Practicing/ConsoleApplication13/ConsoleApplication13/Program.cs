c2c0c38952354784a4f360a737dbdaed3186f48a
Chanuka Dinuwan <40265280+CDinuwan@users.noreply.github.com>
2019-12-16 19:36:45 +0530

    Add files via upload

---
+++ C# Practicing/ConsoleApplication13/ConsoleApplication13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetDay(1));
+            Console.ReadLine();
+        }
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid";
+                    break; 
+            }
+            return dayName;
+        }
+    }
+}
---
+++ C# Practicing/ConsoleApplication14/ConsoleApplication14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            while (index <= 5);
+
+
+            Console.ReadLine();
+        }
+    }
+}
