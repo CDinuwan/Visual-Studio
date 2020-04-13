b3b8551842a19da13cb91f7dcdf9e875e75878e8
Chanuka Dinuwan <40265280+CDinuwan@users.noreply.github.com>
2020-04-13 08:18:59 +0530

    Add files via upload

---
+++ C# Practicing/OOP/OOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Person
+    {
+        private int age;
+        private string name;
+
+        public Person(int age,string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person( 23,"Chanuka");
+            Person person2 = new Person(24, "Dinuwan");
+
+            Console.WriteLine(person1.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Practicing/OOPPrac1/OOPPrac1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrac1
+{
+    public abstract class Laptop
+    {
+        public virtual void TurnOn()
+        {
+            Console.WriteLine("Turn On");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("TurnOff");
+        }public abstract bool TouchScreen
+        {
+            get;
+        }
+    }
+    public class MacBook:Laptop
+    {
+        public override bool TouchScreen => false;
+    }
+    public class AsusZenBook : Laptop
+    {
+        public override bool TouchScreen => true;
+        public override void TurnOn()
+        {
+            Console.WriteLine("Turning On specific to Azus");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop macBook = new MacBook();
+            Laptop azusZenBook = new AsusZenBook();
+
+            macBook.TurnOn();
+            macBook.TurnOff();
+
+            azusZenBook.TurnOn();
+            azusZenBook.TurnOff();
+
+            Console.WriteLine(macBook.TouchScreen);
+            Console.WriteLine(azusZenBook.TouchScreen);
+
+            Console.ReadLine();
+        }
+    }
+}
